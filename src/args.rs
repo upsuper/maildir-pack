98f7d3a9cab585455cc9596920505dfac020826e
Xidorn Quan <me@upsuper.org>
2022-06-07 21:06:33 +1000

    Use derived Parser from clap

--- src/args.rs
+++ src/args.rs
@@ -1,33 +1,25 @@
-use clap::clap_app;
-use std::path::{Path, PathBuf};
+use clap::Parser;
+use std::path::PathBuf;
 
-#[derive(Debug)]
+#[derive(Debug, Parser)]
+#[clap(name = "maildir-pack")]
+#[clap(author, version, about)]
 pub struct Args {
-    /// The maildir we are working on.
+    /// Path to the maildir.
+    #[clap(parse(from_os_str))]
     pub maildir: PathBuf,
     /// The directory we put packed archives in, which is maildir/packed.
+    #[clap(skip)]
     pub packed_dir: PathBuf,
-    /// Whether we should show any progress.
+    /// Suppress any progress output if set.
+    #[clap(short, long, parse(from_flag))]
     pub quiet: bool,
 }
 
 impl Args {
     pub fn parse_args() -> Self {
-        let matches = clap_app!(maildirpack =>
-            (version: env!("CARGO_PKG_VERSION"))
-            (author: "Xidorn Quan <me@upsuper.org>")
-            (about: "Pack mails from a maildir into archives")
-            (@arg MAILDIR: +required "Path to the maildir")
-            (@arg quiet: -q --quiet "Suppress any progress output")
-        )
-        .get_matches();
-
-        let maildir = matches.value_of("MAILDIR").unwrap();
-        let maildir = Path::new(maildir);
-        Args {
-            maildir: maildir.to_path_buf(),
-            packed_dir: maildir.join("packed"),
-            quiet: matches.is_present("quiet"),
-        }
+        let mut result: Self = Self::parse();
+        result.packed_dir = result.maildir.join("packed");
+        result
     }
 }
